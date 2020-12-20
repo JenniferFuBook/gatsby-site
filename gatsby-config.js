d4630a554f52922e5284e5eae7a910679314a795
Jennifer Fu <jennifer.fu@hpe.com>
2020-12-20 11:34:41 -0800

    Fixed image path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: "/github.com/JenniferFuBook/gatsby-site.git",
+  pathPrefix: "/gatsby-site",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
